1b6c3212c4058127acc7da79797873397205373a
pentester <python.malaysia.life@mail.ru>
2026-05-05 14:21:14 +0300

    refactor: remove unused POS profile TypedDict definitions and update PosProfileResponseSerializer to a strict TypedDict

--- fadl_pos/serializers/session.py
+++ fadl_pos/serializers/session.py
@@ -1,71 +1,5 @@
 from typing import TypedDict, List, Optional
 
-class PosProfileUser(TypedDict, total=False):
-    name: str
-    default: int
-    user: str
-
-class PosPaymentMethodNative(TypedDict, total=False):
-    name: str
-    default: int
-    mode_of_payment: str
-    allow_in_returns: int
-
-class PosItemGroup(TypedDict, total=False):
-    name: str
-    item_group: str
-
-class PosCustomerGroup(TypedDict, total=False):
-    name: str
-    customer_group: str
-
-class PosProfileNative(TypedDict, total=False):
-    name: str
-    disabled: int
-    customer: str
-    company: str
-    country: str
-    company_address: str
-    applicable_for_users: List[PosProfileUser]
-    payments: List[PosPaymentMethodNative]
-    item_groups: List[PosItemGroup]
-    customer_groups: List[PosCustomerGroup]
-    letter_head: str
-    tc_name: str
-    select_print_heading: str
-    selling_price_list: str
-    currency: str
-    write_off_account: str
-    write_off_cost_center: str
-    account_for_change_amount: str
-    income_account: str
-    expense_account: str
-    cost_center: str
-    taxes_and_charges: str
-    apply_discount_on: str
-    tax_category: str
-    print_format: str
-    warehouse: str
-    ignore_pricing_rule: int
-    update_stock: int
-    hide_unavailable_items: int
-    hide_images: int
-    auto_add_item_to_cart: int
-    allow_rate_change: int
-    allow_discount_change: int
-    validate_stock_on_save: int
-    write_off_limit: float
-    disable_rounded_total: int
-    utm_campaign: str
-    utm_source: str
-    utm_medium: str
-    print_receipt_on_order_complete: int
-    project: str
-    set_grand_total_to_default_mop: int
-    action_on_new_invoice: str
-    allow_partial_payment: int
-
-
 # --- API Response Serializers ---
 
 class ChecklistSection(TypedDict):
@@ -81,7 +15,7 @@ class PaymentMethodSerializer(TypedDict):
     type: str
     required_ob: bool
 
-class PosProfileResponseSerializer(TypedDict, total=False):
+class PosProfileResponseSerializer(TypedDict):
     name: str
     status: str
     opening_entry: Optional[str]
